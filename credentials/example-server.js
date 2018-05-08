b05b78d18e1cb9f9ec54e3363e17bde43b4f28b3
samwhindleton <samwhindleton@gmail.com>
2018-05-08 07:10:59 -0700

    created example credentials/ files: client.js and server.js

---
+++ credentials/example-server.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'type': 'foobar-account',
+  'project_id': 'foobar',
+  'private_key_id': 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5',
+  'private_key': 'foobar-private-key',
+  'client_email': 'hi@foobar.com',
+  'client_id': '1234567890987654321',
+  'auth_uri': 'https://foobar.example.com/auth',
+  'token_uri': 'https://foobar.example.com/token',
+  'auth_provider_x509_cert_url': 'https://www.example.com/auth/v2/certs',
+  'client_x509_cert_url': 'https://www.example.com/foo/bar/foobar.com'
+}
