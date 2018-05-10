80a736b8c2040e2aad31c1fb5d27746434222967
samwhindleton <samwhindleton@gmail.com>
2018-05-09 17:01:04 -0700

    added meta data

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,26 @@
-import Head from 'next/head';
-import '../styles/main.css';
-import Navbar from './Navbar';
 import Footer from './Footer';
+import Head from 'next/head';
+
+import '../styles/main.css';
 
 const Layout = (props) => (
   <div>
     <Head>
-      <title></title>
+      {/* character encoding */}
+      <meta charSet='utf-8'/>
+      {/* viewport for page dimensions and scaling */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="author" content="Sam Whindleton"/>
+      <meta name="description" content=""/>
+      <meta name="keywords" content=""/>
+      <title>Hey Inspire</title>
+      {/* main css */}
       <link rel="stylesheet" href="/_next/static/style.css"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      {/* font awesome */}
+      <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+      {/* favicon */}
+      {/* <link rel="icon" href="../static/favicon/foobar.ico" type="image/x-icon"/> */}
     </Head>
-    <Navbar/>
     <div className="main-container">
       {props.children}
     </div>
