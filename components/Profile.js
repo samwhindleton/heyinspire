5dc784fddbff1a6e038aeb293b3025aab7659b6c
samwhindleton <samwhindleton@gmail.com>
2018-05-12 19:14:38 -0700

    added user fav images section

--- components/Profile.js
+++ components/Profile.js
@@ -1,44 +1,93 @@
 import React from 'react';
 
+
 export default class Profile extends React.Component {
   // constructor (props) {
   //   super(props)
-  //   this.state = {
-  //     foobar: ''
-  //   }
-  //
-  //   this.foobar = this.foobar.bind(this);
+  //   this.state = {};
   // };
+
 
   render () {
     return (
       <div id="profile">
-        <h1>Profile page.</h1>
-        <img src={this.props.user.photoURL} alt={this.props.user.displayName}/>
-        Welcome {this.props.user.displayName}
+        <div>
+          <h1>Profile page.</h1>
+          <img src={this.props.user.photoURL} alt={this.props.user.displayName}/>
+          Hi {this.props.user.displayName}
+        </div>
 
-        <form onSubmit={this.props.handleSubmit}>
-          <input
-            type='text'
-            onChange={this.props.handleChange}
-            placeholder={'any message here'}
-            value={this.props.value}
-          />
-          <button type='submit'>submit</button>
-        </form>
+        <div>
+          <form onSubmit={this.props.handleSubmit}>
+            <input
+              id='title'
+              type='text'
+              onChange={this.props.handleChange}
+              placeholder='Image description'
+              value={this.props.title}
+            />
+            <input
+              id='value'
+              type='url'
+              onChange={this.props.handleChange}
+              placeholder={'https://behance.net/project/1/my_image.png'}
+              value={this.props.value}
+            />
+            <button type='submit'>submit</button>
+          </form>
+        </div>
 
-        <ul>
-          {
-            this.props.userImages &&
-            Object.keys(this.props.userImages).map(key =>
-              <li key={key}>{this.props.userImages[key].image}
-                <button onClick={() => this.props.handleDelete(key)}>
-                  delete
-                </button>
-              </li>
-            )
-          }
-        </ul>
+        <div>
+          <div>
+            <h1>Image List</h1>
+            <ul>
+              {
+                (this.props.userImages[this.props.user.uid]) ?
+                (
+                  Object.keys(this.props.userImages[this.props.user.uid]).reverse().map
+                    (
+                      key =>
+                        <li key={key}>
+                          <h1>
+                            {this.props.userImages[this.props.user.uid][key].title}
+                          </h1>
+                          <img src={this.props.userImages[this.props.user.uid][key].image} alt=""/>
+                          <button onClick={() => this.props.handleDelete(key)}>
+                            delete
+                          </button>
+                        </li>
+                    )
+                ) :
+                ('')
+              }
+            </ul>
+          </div>
+
+          <div>
+            <h1>Favs list</h1>
+            <ul>
+              {
+                (this.props.userFavImages[this.props.user.uid]) ?
+                (
+                  Object.keys(this.props.userFavImages[this.props.user.uid]).reverse().map
+                  (
+                    key =>
+                      <li key={key}>
+                        <h1>
+                          {this.props.userFavImages[this.props.user.uid][key].title}
+                        </h1>
+                        <img src={this.props.userFavImages[this.props.user.uid][key].image} alt=""/>
+                        <button onClick={() => this.props.handleFavDelete(key)}>
+                          remove
+                        </button>
+                      </li>
+                  )
+                ) :
+                ('')
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
