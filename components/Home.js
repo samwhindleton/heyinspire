954e06661939d2c10b2759a8a0e1e17f3c94e2cc
samwhindleton <samwhindleton@gmail.com>
2018-05-13 01:05:43 -0700

    moved fuctions getImages, randomNum from components/Home.js to pages/index.js

--- components/Home.js
+++ components/Home.js
@@ -1,77 +1,41 @@
 import React from 'react';
 
-// behance
-import behanceCredentials from '../credentials/behance';
-
-// fetch
-import Fetch from 'isomorphic-unfetch';
-
 
 export default class Home extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      projects: [],
-      number: 0,
-      ready: false,
-    };
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {};
+  // };
 
-    this.getImages = this.getImages.bind(this);
-    this.randomNum = this.randomNum.bind(this);
-  };
-
-  componentDidMount () {
-    this.randomNum();
-    this.getImages();
-  };
-
-  getImages () {
-    this.randomNum();
-    fetch(behanceCredentials.cors + behanceCredentials.url + behanceCredentials.client_id)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({
-          projects: data.projects,
-          ready: true
-        });
-        console.log(data.projects);
-        // console.log(this.state.projects[0].name);
-      });
-  };
-
-  // generate random number between 0 - 1
-  randomNum() {
-    this.setState({
-      number: Math.floor(Math.random() * 49)
-    });
-    console.log('random num:', this.state.number);
-  };
 
   render () {
     return (
       <div>
         <h1>Home page.</h1>
 
-        {/* image */}
         {
-          (this.state.ready) ?
+          (this.props.user && this.props.ready) ?
           (
             <div>
-              <a href={this.state.projects[this.state.number].url} target="_blank"><img src={this.state.projects[this.state.number].covers.original} alt={this.state.projects[this.state.number].name}/></a>
-              <h2>{this.state.projects[this.state.number].name}</h2>
+              <a href={this.props.projects[this.props.number].url} target="_blank"><img src={this.props.projects[this.props.number].covers.original} alt={this.props.projects[this.props.number].name}/></a>
+              <h2>{this.props.projects[this.props.number].name}</h2>
             </div>
           ) :
           ('')
         }
 
-        {/* random image button if logged in user */}
         {
           (this.props.user) ?
-          (<button onClick={() => this.randomNum()}>
-            Get Image
-          </button>) :
+          (
+            <div>
+              <button onClick={() => this.props.randomNum()}>
+                Get Image
+              </button>
+              <button onClick={() => this.props.handleFavSubmit()}>
+                Fav
+              </button>
+            </div>
+          ) :
           ('')
         }
       </div>
